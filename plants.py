01ef34c5118df40ca28771bae736e838a3cdfa12
Nicholash Bedi <nicholash602@gmail.com>
2021-09-12 12:20:20 -0400

    moved functionality of testing roation to test class

--- plants.py
+++ plants.py
@@ -4,33 +4,22 @@ import math
 import supporting_math as sm
 import sys
 
-WIDTH = 600
-HEIGHT = 600
-
 class Plants:
     def __init__(self):
+        self.width = 600
+        self.height = 600
+        self.get_plant_img()
+
+    def get_plant_img(self):
         temp = cv.imread("plant_images/leaf.png")
         self.plant_img = np.zeros((temp.shape[0],temp.shape[1], 4))
+        # set RGB of image
         self.plant_img[:, :, :-1] = temp
+        # Set remaining transparency
         non_transparent = self.plant_img[:, :, 0] == 0
         self.plant_img[non_transparent, 3] = 255
-        self.trackbar_window = "trackbar"
-        self.make_trackbars()
 
-    def make_trackbars(self):
-        cv.namedWindow(self.trackbar_window)
-        cv.createTrackbar("x", self.trackbar_window, 300, WIDTH - 60, self.on_trackbar)
-        cv.createTrackbar("y", self.trackbar_window, 300, HEIGHT - 60, self.on_trackbar)
-        cv.createTrackbar("angle", self.trackbar_window, 0, 360, self.on_trackbar)
-
-    def on_trackbar(self, val):
-        pass
-
-    def display_leaf(self):
-        self.base_image = np.full([WIDTH,HEIGHT, 4], 255, dtype=np.uint8)
-        x = cv.getTrackbarPos("x", self.trackbar_window)
-        y = cv.getTrackbarPos("y", self.trackbar_window)
-        angle = cv.getTrackbarPos("angle", self.trackbar_window)
+    def display_leaf(self, x, y, angle):
         rotated_image = sm.rotate_image(self.plant_img, angle, scale = 0.5)
         x_adjust, y_adjust = sm.adjust_image_loc_for_bottom_center_roation(
                                 x, y, self.plant_img.shape[0], angle, scale = 0.5)
@@ -38,8 +27,6 @@ class Plants:
                                         rotated_image, self.base_image)
         cv.imshow("Display window", self.base_image)
 
-p = Plants()
-while True:
-    p.display_leaf()
-    if cv.waitKey(1) & 0xFF == ord('q'):
-          break
+
+if __name__ == "__main__":
+    pass
---
+++ test_plant_roation.py
@@ -0,0 +1,27 @@
+import plants
+import numpy as np
+import cv2 as cv
+
+class TestRotation(plants.Plants):
+    def on_trackbar(self, val):
+        pass
+    def make_trackbars(self):
+        self.trackbar_window = "trackbar"
+        cv.namedWindow(self.trackbar_window)
+        cv.createTrackbar("x", self.trackbar_window, 300, self.width - 60, self.on_trackbar)
+        cv.createTrackbar("y", self.trackbar_window, 300, self.height - 60, self.on_trackbar)
+        cv.createTrackbar("angle", self.trackbar_window, 0, 360, self.on_trackbar)
+
+    def test(self):
+        self.make_trackbars()
+        while True:
+            self.base_image = np.full([self.width,self.height, 4], 255, dtype=np.uint8)
+            x = cv.getTrackbarPos("x", self.trackbar_window)
+            y = cv.getTrackbarPos("y", self.trackbar_window)
+            angle = cv.getTrackbarPos("angle", self.trackbar_window)
+            self.display_leaf(x,y,angle)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                  break
+
+p = TestRotation()
+p.test()
